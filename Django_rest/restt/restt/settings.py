ec9eb611624a9faafceb7e3ee1751e16c37444af
gurudasKarale <67753744+GurudasKarale@users.noreply.github.com>
2021-04-09 16:34:18 +0530

    Update settings.py

--- Django_rest/restt/restt/settings.py
+++ Django_rest/restt/restt/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'rest_database',
         'USER':'postgres',
-        'PASSWORD':'digital8956',
+        'PASSWORD':'',
         'HOST':'localhost',
     }
 }
